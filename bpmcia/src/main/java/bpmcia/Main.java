2177fae774e5f5497ea1c3ba16c6ab9e826c7c13
Acilio Silva <eng.acilio@gmail.com>
2020-04-05 17:27:51 +0100

    Project structer init

---
+++ bpmcia/src/main/java/bpmcia/Main.java
@@ -0,0 +1,16 @@
+package bpmcia;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Main {
+	
+	@PostConstruct
+	public void init() {
+		
+	}
+	
+}
