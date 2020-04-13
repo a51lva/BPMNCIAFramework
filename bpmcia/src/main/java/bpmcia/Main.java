7c0dd6eec21d6aa276a90f1b515681612749f39f
Acilio Silva <eng.acilio@gmail.com>
2020-04-13 01:44:11 +0100

    added new files for layout build

--- bpmcia/src/main/java/bpmcia/Main.java
+++ bpmcia/src/main/java/bpmcia/Main.java
@@ -7,10 +7,17 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class Main {
-	
+	private String appname;
 	@PostConstruct
 	public void init() {
-		
+		setAppname("BPMN Based CIA Framework");		
 	}
 	
+	public String getAppname() {
+		return appname;
+	}
+	
+	public void setAppname(String appname) {
+		this.appname = appname;
+	}
 }
